f4728b9d0c5a100a5535c7f61f1961aef1633fc3
Hilal Ahmad <115874624+hilalahmad0101@users.noreply.github.com>
2025-03-30 22:59:31 -0700

    fix: add another api

--- server.js
+++ server.js
@@ -23,6 +23,14 @@ app.get('/another',(req,res)=>{
 });
 
 
+app.get('/another-api',(req,res)=>{
+    res.status(200).json({
+        'success':true,
+        'message':'This is running another api'
+    });
+});
+
+
 app.listen(3000,()=>{
     console.log('Server is running on port 3000');
 });
