87cebb240622321293c546a41770a30904caa64f
Hilal Ahmad <115874624+hilalahmad0101@users.noreply.github.com>
2025-03-30 22:12:29 -0700

    add user api, to automate cicd

--- server.js
+++ server.js
@@ -8,6 +8,13 @@ app.get('/',(req,res)=>{
     });
 });
 
+app.get('/users',(req,res)=>{
+    res.status(200).json({
+        'success':true,
+        'message':'Get Users Success'
+    });
+});
+
 app.listen(3000,()=>{
     console.log('Server is running on port 3000');
 });
